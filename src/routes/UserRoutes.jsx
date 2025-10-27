7ba4c67c54f9fa964443eb5eca87f7905a65d577
Marlon Abella <Mardavab@gmail.com>
2025-10-26 22:39:50 -0500

    Estilos

--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -7,10 +7,8 @@ import { TopNavbar } from "../components/sidebar/TopNavbar";
 import { UserProvider } from "../context/User/UserProvider";
 import { SalesProvider } from "../context/sales/SalesProvider";
 import { ExpenseProvider } from "../context/expense/ExpenseProvider";
-
 import { ExpenseContent } from "../components/expense/ExpenseContent";
-
-import { SupplierProvider } from "../context/Supplier/SupplierProvider ";
+import { SupplierProvider } from "../context/Supplier/SupplierProvider";
 import { SuppliersContent } from "../components/suppliers/SuppliersContent";
 import { InvoiceProvider } from "../context/invoice/InvoiceContext";
 import { InvoicesContent } from "../components/inovices/InvoicesContent";
@@ -19,63 +17,38 @@ import { ProductsContent } from "../components/product/ProductsContent";
 
 export const UserRoutes = () => {
   return (
-    <>
-      <div id="page-top">
-        <div id="wrapper">
-          <SideBar />
-          <div id="Content-wrapper" className="d-flex flex-column">
-            <div id="Content">
-              <UserProvider>
-                <SalesProvider>
-                  <ExpenseProvider>
-                    
-                      <SupplierProvider>
-                        <InvoiceProvider>
-                          <ProductProvider>
-                            <TopNavbar />
-                            <Routes>
-                              <Route
-                                path="dashboard"
-                                element={<DashboardContent />}
-                              />
-                              <Route
-                                path="/expenses"
-                                element={<ExpenseContent />}
-                              />
-                              <Route
-                                path="/security/users"
-                                element={<UsersContent />}
-                              />
-                              
-                              <Route
-                                path="/purchases/invoices"
-                                element={<InvoicesContent />}
-                              />
-                              <Route
-                                path="/purchases/suppliers"
-                                element={<SuppliersContent />}
-                              />
-                              <Route
-                                path="/inventory"
-                                element={<ProductsContent/>}
-                              />
-                              <Route path="/sales" element={<SaleContent />} />
-                              <Route
-                                path="/"
-                                element={<Navigate to="/dashboard" />}
-                              />
-                            </Routes>
-                          </ProductProvider>
-                        </InvoiceProvider>
-                      </SupplierProvider>
-                    
-                  </ExpenseProvider>
-                </SalesProvider>
-              </UserProvider>
-            </div>
+    <div id="page-top" className="min-vh-100 d-flex flex-column">
+      <div id="wrapper" className="d-flex flex-grow-1">
+        <SideBar />
+
+        <div id="content-wrapper" className="d-flex flex-column flex-grow-1">
+          <div id="content" className="flex-grow-1">
+            <UserProvider>
+              <SalesProvider>
+                <ExpenseProvider>
+                  <SupplierProvider>
+                    <InvoiceProvider>
+                      <ProductProvider>
+                        <TopNavbar />
+                        <Routes>
+                          <Route path="/dashboard" element={<DashboardContent />} />
+                          <Route path="/expenses" element={<ExpenseContent />} />
+                          <Route path="/security/users" element={<UsersContent />} />
+                          <Route path="/purchases/invoices" element={<InvoicesContent />} />
+                          <Route path="/purchases/suppliers" element={<SuppliersContent />} />
+                          <Route path="/inventory" element={<ProductsContent />} />
+                          <Route path="/sales" element={<SaleContent />} />
+                          <Route path="/*" element={<Navigate to="/dashboard" />} />
+                        </Routes>
+                      </ProductProvider>
+                    </InvoiceProvider>
+                  </SupplierProvider>
+                </ExpenseProvider>
+              </SalesProvider>
+            </UserProvider>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
