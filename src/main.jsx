543fd8d3685e2bf32bf3f2f097531973b1959808
Marlon Abella <Mardavab@gmail.com>
2025-10-26 22:29:06 -0500

    bnootsrap local

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import "./Styles.css";
 import {App} from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./auth/context/AuthProvider.jsx";
+import 'startbootstrap-sb-admin-2/css/sb-admin-2.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
